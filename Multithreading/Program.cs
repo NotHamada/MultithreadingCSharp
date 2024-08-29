b9d69d4d0564e3830f3762867ab72ce0d6cf59d9
NotHamada <matheushamada@live.com>
2024-08-29 20:52:20 -0300

    Interface no terminal

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Multithreading.Console;
+﻿using System.Diagnostics;
+
+namespace Multithreading.Console;
+
+using System;
 
 public class Program
 {
@@ -8,13 +12,48 @@ public class Program
         MultiplicacaoDeMatrizes multiplicacaoDeMatrizes = new MultiplicacaoDeMatrizes();
         InversaoDeMatrizes inversaoDeMatrizes = new InversaoDeMatrizes();
 
-        inversaoDeMatrizes.Inversao(1000, false);
-         
+        int opcao = 0;
 
-        //multiplicacaoDeMatrizes.Multiplicacao(100000, true);
+        while (opcao != 4)
+        {
+            Console.WriteLine("Selecione uma das opções abaixo:");
+            Console.WriteLine("1 - Soma de matrizes");
+            Console.WriteLine("2 - Multiplicação de matrizes");
+            Console.WriteLine("3 - Inversão de matriz:");
+            Console.WriteLine("4 - Sair");
+
+            opcao = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("Digite o tamanho da matriz:");
+                    int size = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("0 - Paralela ou 1 - Sequencial?");
+                    bool isSequential = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+                    somaDeMatrizes.Soma(size, isSequential);
+                    break;
+                case 2:
+                    Console.WriteLine("Digite o tamanho da matriz:");
+                    size = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("0 - Paralela ou 1 - Sequencial?");
+                    isSequential = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+                    multiplicacaoDeMatrizes.Multiplicacao(size, isSequential);
+                    break;
+                case 3:
+                    Console.WriteLine("Digite o tamanho da matriz:");
+                    size = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("0 - Paralela ou 1 - Sequencial?");
+                    isSequential = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+                    inversaoDeMatrizes.Inversao(size, isSequential);
+                    break;
+                case 4:
+                    break;
+            }
+        }
+
+
+        //multiplicacaoDeMatrizes.Multiplicacao(10000, true);
         //somaDeMatrizes.Soma(10000, true);
-        
-        multiplicacaoDeMatrizes.Multiplicacao(10000, true);
-        somaDeMatrizes.Soma(10000, true);
     }
 }
